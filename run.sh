8f56a6428a3751dfe496c0d2e0ce7eb31a26cef0
ZhangH <60123620+fff1f@users.noreply.github.com>
2023-08-10 00:39:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 python main.py \
---data_path ijcai2016seq100.csv \
+--data_path seq_yelp.csv \
 --max_len 100 \
---save_path save/taobaot/ \
+--save_path save/ \
 --lr 1e-3 \
 --train_batch_size 128 \
---test_batch_size 64 \
+--test_batch_size 256 \
 --attn_heads 4 \
 --dropout 0.1 \
 --d_ffn 256 \
 --d_model 128 \
 --bert_layers 2 \
---ld1 $d \
---ld2 1e-3 \
+--ld1 1e-2 \
+--ld2 1e-2 \
 --enable_res_parameter 1 \
---device cuda:3 \
+--device cuda:0 \
 --num_epoch 20 \
---baserate 2 \
---seed 2022
-
-
+--baserate 1 \
+--t 5 \
+--gamma 0.9 \
+--seed 0
